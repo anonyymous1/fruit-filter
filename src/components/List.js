c2fd8480a276250a4f46890487451fc27d42506c
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-12-10 11:41:53 -0500

    [List.js] Add class component

---
+++ src/components/List.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    constructor(props){
+        super()
+        this.state = {};
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default List;
